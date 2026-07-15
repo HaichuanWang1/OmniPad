44755604f398d6d492d02d5f470768853f4c4054
OmniPad Developer <developer@omnipad.dev>
2026-07-15 19:36:55 +0800

    Client: 所有仓库切换为阿里云国内镜像源
    - settings.gradle.kts: pluginManagement + dependencyResolutionManagement 均添加 aliyun mirror
    - gradle-wrapper.properties: distributionUrl 指向 aliyun gradle-distributions
    - build.gradle.kts: AGP 8.2.2, Kotlin 1.9.22
    - app/build.gradle.kts: Compose BOM 2024.02.00

--- client/settings.gradle.kts
+++ client/settings.gradle.kts
@@ -1,13 +1,19 @@
 pluginManagement {
     repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/public/") }
+        maven { setUrl("https://maven.aliyun.com/repository/google/") }
         google()
         mavenCentral()
     }
